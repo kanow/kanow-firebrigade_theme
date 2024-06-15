db553c9d3a6c35fcf4d074cb07f8668a7c57507c
Karsten Nowak <captnnowi@gmx.de>
2024-06-15 16:19:39 +0200

    [TASK] Add php-cs-fixer config

---
+++ php-cs-fixer.php
@@ -0,0 +1,9 @@
+<?php
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use TYPO3\CodingStandards\CsFixerConfig;
+
+$config = CsFixerConfig::create();
+$config->setParallelConfig(ParallelConfigFactory::detect());
+$config->getFinder()->in('Configuration');
+return $config;
